1c9f47d2439c546b9690d24571b91e517578c143
Souvik Mitra <msouvik38@gmail.com>
2021-04-08 20:02:03 +1000

    adding navigation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,10 @@ import { PhoneMaskDirective } from '../directives/phone-mask.directive';
 import { ForgotPasswordComponent } from '../components/forgot-password/forgot-password.component';
 import { MyProfileComponent } from '../components/my-profile/my-profile.component';
 import { HomeComponent } from '../components/home/home.component';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatIconModule } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from '../components/navigation/header/header.component';
 import { SidenavListComponent } from '../components/navigation/sidenav-list/sidenav-list.component';
-import { MatMenuModule } from '@angular/material/menu';
 import { LayoutComponent } from '../components/layout/layout.component';
+import { NavModule } from './nav.module';
 
 @NgModule({
   declarations: [
@@ -41,24 +34,7 @@ import { LayoutComponent } from '../components/layout/layout.component';
     AppRoutingModule,
     ReactiveFormsModule,
     NgbModule,
-    MatListModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatTabsModule,
-    MatIconModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule
-  ],
-  exports: [
-    MatListModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatTabsModule,
-    MatIconModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule
+    NavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
